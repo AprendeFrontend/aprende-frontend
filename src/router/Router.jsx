21fa4c39ce60e8b5591c18b6aed02aebd188a897
Dorian <aprendefrontend@gmail.com>
2025-01-11 00:19:14 +0100

    updated

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from '../layouts/Layout';
 import Challenges from '../pages/challenges/Challenges';
 import Home from '../pages/home/Home';
-import Profile from '../pages/Profile/Profile';
+import Profile from '../pages/profile/Profile';
 import ProtectedRoute from './ProtectedRoute';
 
 const Router = () => {
